078c3b2e62b787914698ff1c0d952a3f9e7fca25
Pritam <pritamdebnath297@gmail.com>
2021-10-22 22:19:43 +0530

    feat: added navbar component to main page

--- components/logo.js
+++ components/logo.js
@@ -18,7 +18,22 @@ padding: 10px;
 `
 const Logo=()=>{
     const footPrintImg = `/images/footprint${useColorModeValue('','-dark')}.png`
-    return()
+    return(
+        <Link href="/">
+            <a>
+                <LogoBox>
+                    <Image src={footPrintImg} width={20} height={20} alt="logo"/>
+                    <Text 
+                        color={useColorModeValue('gray.800','whiteAlpha.900')}
+                        fontFamily='M PLUS Rounded 1c'
+                        fontWeight="bold"
+                        ml={3}>
+                        Pritam Debnath
+                    </Text>
+                </LogoBox>
+            </a>
+        </Link>
+    )
 }
 
 export default Logo;
