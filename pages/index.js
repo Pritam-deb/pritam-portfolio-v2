ace59209e10ed195c216add23cd3c0fc299eb546
Pritam <pritamdebnath297@gmail.com>
2021-10-24 09:47:36 +0530

    feat: added profile picture

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container,Box,Heading } from "@chakra-ui/react"
+import { Container,Box,Heading,Image,useColorModeValue } from "@chakra-ui/react"
 
 const Home = () => {
     return(
         <Container>
-            <Box borderRadius="lg" bg="red" p={3}  mb={6} textAlign="center">
+            <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500','whiteAlpha.200')} p={3}  mb={6} textAlign="center" borderTop="10px">
                 Hello, I&apos;m a full-stack developer based in India!
             </Box>
             <Box display={{md: 'flex'}}>
@@ -12,6 +12,9 @@ const Home = () => {
                         Pritam Debnath
                     </Heading>
                     <p>Digital Craftsman(Developer / Artist / Desginer)</p>
+                </Box>
+                <Box flexShrink={0} mt={{base:4,md:0}} ml={{md:6}} align="center">
+                    <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" display="inline-block" borderRadius="full" src="/images/pritam.jpg" alt="profile image" />
                 </Box>
             </Box>
         </Container>
