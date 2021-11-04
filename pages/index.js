bceb6ee9e530ea2ecd1aeac1ca32e56f44de97f8
Pritam <pritamdebnath297@gmail.com>
2021-11-04 11:25:23 +0530

    feat: implemented page transition
    used motion frameworks

--- pages/index.js
+++ pages/index.js
@@ -10,95 +10,104 @@ import {
 import NextLink from 'next/link'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
+import Layout from '../components/layouts/article'
 import { BioSection, BioYear } from '../components/bio'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 const Home = () => {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        p={3}
-        mb={6}
-        textAlign="center"
-      >
-        Hello, I&apos;m a full-stack developer based in India!
-      </Box>
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
-            Pritam Debnath
-          </Heading>
-          <p>Digital Craftsman(Developer / Artist / Desginer)</p>
-        </Box>
+    <Layout>
+      <Container>
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          align="center"
+          borderRadius="lg"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          p={3}
+          mb={6}
+          textAlign="center"
         >
-          <Image
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            maxWidth="100px"
-            display="inline-block"
-            borderRadius="full"
-            src="/images/pritam.jpg"
-            alt="profile image"
-          />
+          Hello, I&apos;m a full-stack developer based in India!
         </Box>
-      </Box>
-      <Section delay={0.1}>
-        <Heading as="h3" variant="section-title">
-          Work
-        </Heading>
-        <Paragraph padding="10px">
-          Pritam is a freelance and a full-stack developer based in Shillong
-          with a passion for building digital services/stuff he wants. He has a
-          knack for all things launching products, from planning and designing
-          all the way to solving real-life problems with code. When not online,
-          he loves hanging out with his friends or reading books. Currently, he
-          is working in {''}
-          <NextLink href="https://www.geekyants.com/">
-            <Link>GeekyAnts</Link>
-          </NextLink>
-          .
-        </Paragraph>
-        <Box align="center" my={4}>
-            <NextLink href="/works">
-                <Button rightIcon={<ChevronRightIcon/>} colorScheme="teal">My Portfolio</Button>
-            </NextLink>
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2" variant="page-title">
+              Pritam Debnath
+            </Heading>
+            <p>Digital Craftsman(Developer / Artist / Desginer)</p>
+          </Box>
+          <Box
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            align="center"
+          >
+            <Image
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              maxWidth="100px"
+              display="inline-block"
+              borderRadius="full"
+              src="/images/pritam.jpg"
+              alt="profile image"
+            />
+          </Box>
         </Box>
-      </Section>
-      <Section delay={0.5}>
+        <Section delay={0.1}>
           <Heading as="h3" variant="section-title">
-              Bio
+            Work
+          </Heading>
+          <Paragraph padding="10px">
+            Pritam is a freelance and a full-stack developer based in Shillong
+            with a passion for building digital services/stuff he wants. He has
+            a knack for all things launching products, from planning and
+            designing all the way to solving real-life problems with code. When
+            not online, he loves hanging out with his friends or reading books.
+            Currently, he is working in {''}
+            <NextLink href="https://www.geekyants.com/">
+              <Link>GeekyAnts</Link>
+            </NextLink>
+            .
+          </Paragraph>
+          <Box align="center" my={4}>
+            <NextLink href="/works">
+              <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                My Portfolio
+              </Button>
+            </NextLink>
+          </Box>
+        </Section>
+        <Section delay={0.5}>
+          <Heading as="h3" variant="section-title">
+            Bio
           </Heading>
           <BioSection>
-              <BioYear>1998</BioYear>
-              Born in Shillong(Meghalaya), India.
+            <BioYear>1998</BioYear>
+            Born in Shillong(Meghalaya), India.
           </BioSection>
           <BioSection>
-              <BioYear>2018</BioYear>
-              Completed Higher Secondary with Science and Computer Science major from St. Anthony&apos;s Higher Secondary School
+            <BioYear>2018</BioYear>
+            Completed Higher Secondary with Science and Computer Science major
+            from St. Anthony&apos;s Higher Secondary School
           </BioSection>
           <BioSection>
-              <BioYear>2021</BioYear>
-              Completed Bachelor&apos;s in Computer Applications at Acharya Institute of Graduate Studies
+            <BioYear>2021</BioYear>
+            Completed Bachelor&apos;s in Computer Applications at Acharya
+            Institute of Graduate Studies
           </BioSection>
-      </Section>
-      <Section delay={1}>
+        </Section>
+        <Section delay={1}>
           <Heading as="h3" variant="section-title">
-              I ♥
+            I ♥
           </Heading>
           <Paragraph>
             MMA, Boxing, {''}
-            <Link href="https://www.goodreads.com/review/list/90017901-pritam-debnath?ref=nav_mybooks&shelf=read">Books</Link>
+            <Link href="https://www.goodreads.com/review/list/90017901-pritam-debnath?ref=nav_mybooks&shelf=read">
+              Books
+            </Link>
             , Drawing, Animation
           </Paragraph>
-      </Section>
-    </Container>
+        </Section>
+      </Container>
+    </Layout>
   )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,23 +1,31 @@
-import { Container, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
-import Section from "../components/section";
-import { WorkGridItem } from "../components/grid-item";
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-item'
 import thumbCryptoCoin from '../public/images/CryptoCoin.png'
+import Layout from '../components/layouts/article'
 
-const Works = () =>{
-    return(
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Works 
-            </Heading>
-            <SimpleGrid columns={[1,1,2]} gap={6}>
-                <Section>
-                    <WorkGridItem id="cryptocoin" title="CryptoCoin" thumbnail={thumbCryptoCoin}>
-                        A webapp that displays the real-time price of cryptocurrencies. This fetches data from an API.
-                    </WorkGridItem>
-                </Section>
-            </SimpleGrid>
-        </Container>
-    )
+const Works = () => {
+  return (
+    <Layout>
+      <Container>
+        <Heading as="h3" fontSize={20} mb={4}>
+          Works
+        </Heading>
+        <SimpleGrid columns={[1, 1, 2]} gap={6}>
+          <Section>
+            <WorkGridItem
+              id="cryptocoin"
+              title="CryptoCoin"
+              thumbnail={thumbCryptoCoin}
+            >
+              A webapp that displays the real-time price of cryptocurrencies.
+              This fetches data from an API.
+            </WorkGridItem>
+          </Section>
+        </SimpleGrid>
+      </Container>
+    </Layout>
+  )
 }
 
-export default Works;
+export default Works
